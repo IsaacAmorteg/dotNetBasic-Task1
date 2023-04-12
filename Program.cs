107d166183caac513d4cebc321bcc47da6026ada
Isaac Amortegui <isaacamortegc@outlook.com>
2023-04-12 17:10:19 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a word: ");
+        string inputWord = Console.ReadLine();
+
+        string[] words = inputWord.Split(' ');
+
+        foreach (string word in words) 
+        {
+          string cleanWord = CleanWord(word);
+            Console.WriteLine(cleanWord);
+        }
+    }
+
+    static string CleanWord(string word)
+    {
+        string cleanWord = "";
+
+        foreach (char c in word)
+        {
+            if (Char.IsLetter(c))
+            {
+                cleanWord += Char.ToLower(c);
+            }
+        }
+
+        return cleanWord;
+    }
+}
